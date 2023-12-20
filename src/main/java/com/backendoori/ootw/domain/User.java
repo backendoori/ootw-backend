9bb5fa83c5fb4cbe6b8f2eaea0620041df9e0f43
shoeone96 <dlwnddnjs96@gmail.com>
2023-12-20 15:01:13 +0900

    feat: schema 기반 초기 entity 세팅

---
+++ src/main/java/com/backendoori/ootw/domain/User.java
@@ -0,0 +1,36 @@
+package com.backendoori.ootw.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "nickname", nullable = false)
+    private String nickname;
+
+    @Column(name = "image")
+    private String image;
+
+}
