b6c511e92f696f37e89ec94c29ac04f17803a1ed
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-21 19:01:56 +0900

    style: checkstyle에 맞도록 code style로 정렬

--- src/main/java/com/backendoori/ootw/domain/BaseEntity.java
+++ src/main/java/com/backendoori/ootw/domain/BaseEntity.java
@@ -1,12 +1,14 @@
 package com.backendoori.ootw.domain;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
-import lombok.Getter;
+
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 
 @Getter
 @MappedSuperclass
