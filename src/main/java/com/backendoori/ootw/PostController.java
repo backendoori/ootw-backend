11f08780f4a14045b8dc962fe6f0d7060d66f1de
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-23 11:19:47 +0900

    feat: 게시글(Post) 관련 기능을 구현할 Controller 레이어 생성

---
+++ src/main/java/com/backendoori/ootw/PostController.java
@@ -0,0 +1,14 @@
+package com.backendoori.ootw;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/posts")
+@RestController
+public class PostController {
+
+    private final PostService postService;
+
+}
