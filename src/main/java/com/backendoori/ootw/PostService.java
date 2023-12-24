14e9d7548d5faa986e4cb66c67dc5a999324acd7
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-24 21:20:12 +0900

    feat: 게시글 생성에 대한 Service 레이어 로직 추가

--- src/main/java/com/backendoori/ootw/PostService.java
+++ src/main/java/com/backendoori/ootw/PostService.java
@@ -1,5 +1,12 @@
 package com.backendoori.ootw;
 
+import java.util.NoSuchElementException;
+import com.backendoori.ootw.domain.Post;
+import com.backendoori.ootw.domain.User;
+import com.backendoori.ootw.dto.PostSaveRequest;
+import com.backendoori.ootw.dto.PostSaveResponse;
+import com.backendoori.ootw.repository.PostRepository;
+import com.backendoori.ootw.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,5 +15,17 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public PostSaveResponse save(PostSaveRequest request) {
+        // TODO: 사용자 인증/인가 로직 추가
+        User user = userRepository.findById(request.userId())
+            .orElseThrow(() ->
+                new NoSuchElementException("해당하는 유저가 없습니다.")
+            );
+
+        Post savedPost = postRepository.save(Post.from(user, request));
+        return PostSaveResponse.from(savedPost);
+    }
 
 }
