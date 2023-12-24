3b2e47c5aac2d1ccec6e81ca98f8e53e2e559397
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-24 20:50:44 +0900

    refactor: 게시글 생성 요청 dto 구현

---
+++ src/main/java/com/backendoori/ootw/dto/PostSaveRequest.java
@@ -0,0 +1,19 @@
+package com.backendoori.ootw.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record PostSaveRequest(
+    @NotNull
+    Long userId,
+    @NotBlank
+    @Size(max = 30)
+    String title,
+    @NotBlank
+    @Size(max = 500)
+    String content,
+    String image
+) {
+
+}
