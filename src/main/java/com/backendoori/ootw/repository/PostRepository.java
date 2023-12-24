bb30f074aea2b2190fdb4db05c847435c0027ccb
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-24 22:41:58 +0900

    feat: postId로 Post 단건 조회 EntityGraph 이용하여 구현

--- src/main/java/com/backendoori/ootw/repository/PostRepository.java
+++ src/main/java/com/backendoori/ootw/repository/PostRepository.java
@@ -1,8 +1,16 @@
 package com.backendoori.ootw.repository;
 
+import java.util.Optional;
 import com.backendoori.ootw.domain.Post;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @EntityGraph(attributePaths = "user")
+    @Query("select p from Post p where p.id = :postId")
+    Optional<Post> findByIdWithEntityGraph(@Param("postId") Long postId);
 
 }
