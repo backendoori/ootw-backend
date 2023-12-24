fefce9482bd5ba1019295116f49dd1598b958d38
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-24 21:21:24 +0900

    feat: 게시글 생성에 대한 Controller 엔드포인트 구현

--- src/main/java/com/backendoori/ootw/PostController.java
+++ src/main/java/com/backendoori/ootw/PostController.java
@@ -1,6 +1,14 @@
 package com.backendoori.ootw;
 
+import java.net.URI;
+import com.backendoori.ootw.dto.PostSaveRequest;
+import com.backendoori.ootw.dto.PostSaveResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 
     private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<PostSaveResponse> save(@RequestBody @Valid PostSaveRequest request) {
+
+        PostSaveResponse response = postService.save(request);
+
+        return ResponseEntity.created(URI.create("/api/v1/posts/" + response.getPostId()))
+            .contentType(MediaType.APPLICATION_JSON)
+            .body(response);
+    }
 
 }
