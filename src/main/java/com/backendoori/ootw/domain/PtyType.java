95406a97f8ed4f9f8446f1503ae708bc71c26942
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-26 00:49:37 +0900

    feat:강수상태를 표현하는 PtyType enum 클래스 구현

---
+++ src/main/java/com/backendoori/ootw/domain/PtyType.java
@@ -0,0 +1,32 @@
+package com.backendoori.ootw.domain;
+
+import java.util.Arrays;
+import java.util.Objects;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum PtyType {
+
+    NONE(0),
+    RAIN(1),
+    RAIN_OR_SNOW(2),
+    SNOW(3),
+    SHOWER(4);
+
+    private final Integer code;
+
+    public static PtyType getByCode(Integer code) {
+        return Arrays.stream(values())
+            .filter(skyType -> skyType.isCodeMatch(code))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("강수상태 코드가 유효하지 않은 번호입니다."));
+    }
+
+    public boolean isCodeMatch(Integer code) {
+        return Objects.equals(this.code, code);
+    }
+
+}
