b22218ecf0e4cfa028aaeed95c23f78652991b24
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-26 01:01:52 +0900

    feat: 기온과 날씨 정보를 담고 있는 WeatherInfo dto 클래스 구현

---
+++ src/main/java/com/backendoori/ootw/dto/WeatherInfo.java
@@ -0,0 +1,45 @@
+package com.backendoori.ootw.dto;
+
+import com.backendoori.ootw.domain.Weather;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record WeatherInfo(
+    @NotNull
+    @DecimalMin(value = "-900.0", inclusive = false)
+    @DecimalMax(value = "900.0", inclusive = false)
+    Double currentTemperature,
+
+    @NotNull
+    @DecimalMin(value = "-900.0", inclusive = false)
+    @DecimalMax(value = "900.0", inclusive = false)
+    Double dayMinTemperature,
+
+    @NotNull
+    @DecimalMin(value = "-900.0", inclusive = false)
+    @DecimalMax(value = "900.0", inclusive = false)
+    Double dayMaxTemperature,
+
+    @NotNull
+    @Positive
+    Integer skyCode,
+
+    @NotNull
+    @PositiveOrZero
+    Integer ptyCode
+) {
+
+    public static WeatherInfo from(Weather weather) {
+        return new WeatherInfo(
+            weather.getCurrentTemperature().getValue(),
+            weather.getDayMinTemperature().getValue(),
+            weather.getDayMaxTemperature().getValue(),
+            weather.getSkyType().getCode(),
+            weather.getPtyType().getCode()
+        );
+    }
+
+}
