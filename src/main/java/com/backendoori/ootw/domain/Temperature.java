b71a26f6218277e2f333f90cb172d6d94e699eed
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-26 00:48:19 +0900

    feat:온도를 표현하는 Temperature wrapper 클래스 생성

---
+++ src/main/java/com/backendoori/ootw/domain/Temperature.java
@@ -0,0 +1,28 @@
+package com.backendoori.ootw.domain;
+
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Embeddable
+public class Temperature {
+
+    private static final Double MIN_VALUE = -900.0;
+    private static final Double MAX_VALUE = 900.0;
+    private Double value;
+
+    protected Temperature(Double value) {
+        validate(value);
+        this.value = value;
+    }
+
+    public static void validate(Double value) {
+        if (MIN_VALUE >= value || value >= MAX_VALUE) {
+            throw new IllegalArgumentException("온도는 -900 이하, 900 이상이 될 수 없습니다.");
+        }
+    }
+
+}
