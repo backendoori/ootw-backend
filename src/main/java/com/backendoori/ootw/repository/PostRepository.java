f9f54329bffc12cf78aca8bf1b125a7e0c738658
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-25 15:41:13 +0900

    feat: EntityGraph를 이용한 Post 목록 조회 메서드 작성

--- src/main/java/com/backendoori/ootw/repository/PostRepository.java
+++ src/main/java/com/backendoori/ootw/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.backendoori.ootw.repository;
 
+import java.util.List;
 import java.util.Optional;
 import com.backendoori.ootw.domain.Post;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -11,6 +12,10 @@ public interface PostRepository extends JpaRepository<Post, Long> {
 
     @EntityGraph(attributePaths = "user")
     @Query("select p from Post p where p.id = :postId")
-    Optional<Post> findByIdWithEntityGraph(@Param("postId") Long postId);
+    Optional<Post> findByIdWithUser(@Param("postId") Long postId);
+
+    @EntityGraph(attributePaths = "user")
+    @Query("select p from Post p order by p.createdAt desc")
+    List<Post> findAllWithUser();
 
 }
