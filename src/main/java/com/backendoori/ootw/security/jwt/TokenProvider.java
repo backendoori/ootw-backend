d559a95240a65bd02603a8860f316c2844e470c1
ASak1104 <khw56184@gmail.com>
2023-12-27 07:30:44 +0900

    refactor: 밀리 세컨드 변환을 상수로 사용

--- src/main/java/com/backendoori/ootw/security/jwt/TokenProvider.java
+++ src/main/java/com/backendoori/ootw/security/jwt/TokenProvider.java
@@ -3,20 +3,26 @@ package com.backendoori.ootw.security.jwt;
 import javax.crypto.SecretKey;
 import java.util.Collections;
 import java.util.Date;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import io.jsonwebtoken.security.MacAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class TokenProvider {
 
     public static final String USER_ID_CLAIM = "user_id";
+    public static final int MILLS = 1000;
 
     private final long tokenValidityInMilliseconds;
     private final String issuer;
@@ -27,7 +33,7 @@ public class TokenProvider {
     public TokenProvider(JwtProperties jwtProperties) {
         byte[] keyBytes = Decoders.BASE64.decode(jwtProperties.base64Secret());
 
-        this.tokenValidityInMilliseconds = jwtProperties.tokenValidityInSeconds() * 1000;
+        this.tokenValidityInMilliseconds = jwtProperties.tokenValidityInSeconds() * MILLS;
         this.issuer = jwtProperties.issuer();
         this.algorithm = Jwts.SIG.HS512;
         this.secretKey = Keys.hmacShaKeyFor(keyBytes);
@@ -61,10 +67,24 @@ public class TokenProvider {
         try {
             jwtParser.parseSignedClaims(token);
         } catch (JwtException | IllegalArgumentException e) {
+            loggingException(e);
+
             return false;
         }
 
         return true;
+    }
+
+    private void loggingException(RuntimeException e) {
+        if (e instanceof SecurityException || e instanceof MalformedJwtException) {
+            log.debug("Invalid JWT signature.", e);
+        } else if (e instanceof ExpiredJwtException) {
+            log.debug("Expired JWT token.", e);
+        } else if (e instanceof UnsupportedJwtException) {
+            log.debug("Unsupported JWT token.", e);
+        } else {
+            log.debug("JWT token compact of handler are invalid.", e);
+        }
     }
 
 }
