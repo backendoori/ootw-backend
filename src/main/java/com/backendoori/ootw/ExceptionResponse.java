badecb4340847895a94313baaed076664f6fd8f7
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-26 10:52:47 +0900

    enhance: BindingResult 관련된 예외 응답과 그 외 예외 구분해서 구현

--- src/main/java/com/backendoori/ootw/ExceptionResponse.java
+++ src/main/java/com/backendoori/ootw/ExceptionResponse.java
@@ -1,12 +1,37 @@
 package com.backendoori.ootw;
 
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+
 public record ExceptionResponse<T>(
-    Class exceptionClass,
-    String exceptionMessage
+    T error
 ) {
 
-    public static <T> ExceptionResponse<T> from(T e) {
-        return new ExceptionResponse<T>(e.getClass(), e.toString());
+    public static ExceptionResponse<List<FieldErrorDetail>> from(
+        MethodArgumentNotValidException e) {
+        List<FieldErrorDetail> errors = e.getBindingResult()
+            .getFieldErrors()
+            .stream()
+            .map(fieldError ->
+                new FieldErrorDetail(fieldError.getField(), fieldError.getDefaultMessage()))
+            .toList();
+        return new ExceptionResponse<>(errors);
+    }
+
+    public static <E extends Exception> ExceptionResponse<String> from(E e) {
+        return new ExceptionResponse<>(e.getMessage());
+    }
+
+    @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+    @Getter
+    public static class FieldErrorDetail {
+
+        private final String field;
+        private final String defaultMessage;
+
     }
 
 }
