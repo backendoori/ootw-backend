52be81fb06af67c1acaf0b394d449c0bdf1135e0
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2023-12-30 14:02:33 +0900

    refactor: AvatarItemController에 @RequestMapping("/api/v1/image") 설정

--- src/main/java/com/backendoori/ootw/avtar/controller/AvatarItemController.java
+++ src/main/java/com/backendoori/ootw/avtar/controller/AvatarItemController.java
@@ -8,17 +8,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping("/api/v1/image")
 @RequiredArgsConstructor
 public class AvatarItemController {
 
     private final AvatarItemService appearanceService;
 
-    @PostMapping("/api/v1/image")
+    @PostMapping
     public ResponseEntity<AvatarItemResponse> uploadImage(@RequestPart("file") MultipartFile file,
                                                           @RequestBody AvatarItemRequest requestDto) {
         AvatarItemResponse avatarItem = appearanceService.uploadItem(file, requestDto);
