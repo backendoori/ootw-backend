dd5b6e31c2dcc4a7d66cf53ec73b1a044fe81c14
ASak1104 <khw56184@gmail.com>
2024-01-03 01:44:57 +0900

    refactor: minio config 프로퍼티 분리

--- src/main/java/com/backendoori/ootw/config/MiniOConfig.java
+++ src/main/java/com/backendoori/ootw/config/MiniOConfig.java
@@ -3,44 +3,40 @@ package com.backendoori.ootw.config;
 import io.minio.BucketExistsArgs;
 import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
-import jakarta.annotation.PostConstruct;
-import lombok.Getter;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
-
-@Slf4j
-@Getter
 @Component
+@RequiredArgsConstructor
 public class MiniOConfig {
 
-    @Value("${minio.url}")
-    private String url;
-
-    @Value("${minio.access-key}")
-    private String accessKey;
-
-    @Value("${minio.secret-key}")
-    private String accessSecret;
-
-    @Value("${minio.bucket}")
-    private String bucket;
+    private final MiniOProperties miniOProperties;
 
     @Bean
     public MinioClient minioClient() throws Exception {
         MinioClient minioClient = MinioClient.builder()
-            .endpoint(url)
-            .credentials(accessKey, accessSecret)
+            .endpoint(miniOProperties.url())
+            .credentials(miniOProperties.accessKey(), miniOProperties.secretKey())
             .build();
-        boolean found = minioClient.bucketExists(BucketExistsArgs.builder().bucket(bucket).build());
-        if (!found) {
-            minioClient.makeBucket(MakeBucketArgs.builder().bucket(bucket).build());
+
+        if (!isBucketExists(minioClient)) {
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket(miniOProperties.bucket()).build());
         }
 
         return minioClient;
     }
 
+    public String getBucket() {
+        return miniOProperties.bucket();
+    }
+
+    private boolean isBucketExists(MinioClient minioClient) throws Exception {
+        BucketExistsArgs bucketExistsArgs = BucketExistsArgs.builder()
+            .bucket(miniOProperties.bucket())
+            .build();
+
+        return minioClient.bucketExists(bucketExistsArgs);
+    }
 
 }
