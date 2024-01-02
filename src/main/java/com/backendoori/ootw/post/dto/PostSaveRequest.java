4191081fc5ce1a2fdd4768451d31b1b7a5fe94fb
ASak1104 <khw56184@gmail.com>
2024-01-03 06:07:46 +0900

    feat: 불필요한 record 필드 삭제

--- src/main/java/com/backendoori/ootw/post/dto/PostSaveRequest.java
+++ src/main/java/com/backendoori/ootw/post/dto/PostSaveRequest.java
@@ -1,13 +1,9 @@
 package com.backendoori.ootw.post.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record PostSaveRequest(
-    @NotNull
-    Long userId,
-
     @NotBlank
     @Size(max = 30)
     String title,
