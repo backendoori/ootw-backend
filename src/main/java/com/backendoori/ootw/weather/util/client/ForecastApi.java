577139de3408aa56cd3da8760af75acbd15e8831
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-03 17:32:19 +0900

    feat: 초단기예보와 단기예보를 불러올 수 있는 클라이언트 인터페이스 개발

---
+++ src/main/java/com/backendoori/ootw/weather/util/client/ForecastApi.java
@@ -0,0 +1,65 @@
+package com.backendoori.ootw.weather.util.client;
+
+import com.backendoori.ootw.weather.dto.forecast.ForecastResult;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "forecast-api", url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0")
+public interface ForecastApi {
+
+    @GetMapping(value = "/getVilageFcst")
+    ForecastResult getVilageForecast(
+        @RequestParam
+        String serviceKey,
+
+        @RequestParam
+        int numOfRows,
+
+        @RequestParam
+        int pageNo,
+
+        @RequestParam
+        String dataType,
+
+        @RequestParam(name = "base_date")
+        String baseDate,
+
+        @RequestParam(name = "base_time")
+        String baseTime,
+
+        @RequestParam
+        int nx,
+
+        @RequestParam
+        int ny
+    );
+
+    @GetMapping(value = "/getUltraSrtFcst")
+    ForecastResult getUltraShortForecast(
+        @RequestParam
+        String serviceKey,
+
+        @RequestParam
+        int numOfRows,
+
+        @RequestParam
+        int pageNo,
+
+        @RequestParam
+        String dataType,
+
+        @RequestParam(name = "base_date")
+        String baseDate,
+
+        @RequestParam(name = "base_time")
+        String baseTime,
+
+        @RequestParam
+        int nx,
+
+        @RequestParam
+        int ny
+    );
+
+}
