cd7674f29e045591e0b349b894df6ae3c656b40d
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-03 19:52:10 +0900

    feat: 현재 날씨 정보를 불러오는 dto에 생성자 로직 추가

--- src/main/java/com/backendoori/ootw/weather/dto/WeatherResponse.java
+++ src/main/java/com/backendoori/ootw/weather/dto/WeatherResponse.java
@@ -1,6 +1,8 @@
 package com.backendoori.ootw.weather.dto;
 
 import java.time.LocalDateTime;
+import java.util.Map;
+import com.backendoori.ootw.weather.domain.ForecastCategory;
 
 public record WeatherResponse(
     LocalDateTime currentDateTime,
@@ -10,5 +12,15 @@ public record WeatherResponse(
     Integer nx,
     Integer ny
 ) {
+
+    public static WeatherResponse from(LocalDateTime dateTime, int nx, int ny,
+                                       Map<ForecastCategory, String> currentWeather) {
+
+        Double currentTemperature = Double.valueOf(currentWeather.get(ForecastCategory.T1H));
+        Integer skyCode = Integer.valueOf(currentWeather.get(ForecastCategory.SKY));
+        Integer ptyCode = Integer.valueOf(currentWeather.get(ForecastCategory.PTY));
+
+        return new WeatherResponse(dateTime, currentTemperature, skyCode, ptyCode, nx, ny);
+    }
 
 }
