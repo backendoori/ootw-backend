32960b727e6008eebff6028ac4cf4e4ed7cb448f
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-04 10:38:38 +0900

    chore: 기상청 api url env 변수로 적용

--- src/main/java/com/backendoori/ootw/weather/util/client/ForecastApi.java
+++ src/main/java/com/backendoori/ootw/weather/util/client/ForecastApi.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "forecast-api", url = "http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0")
+@FeignClient(value = "forecast-api", url = "${forecast.url}")
 public interface ForecastApi {
 
     @GetMapping(value = "/getVilageFcst")
