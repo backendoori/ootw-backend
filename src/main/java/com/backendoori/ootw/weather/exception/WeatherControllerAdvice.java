249e147ee5039e6a01dfbc7074ae4a5e2d13b308
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-06 15:54:00 +0900

    refactor: 날씨 컨트롤러 어드바이스 메서드명 변경 및 로깅

--- src/main/java/com/backendoori/ootw/weather/exception/WeatherControllerAdvice.java
+++ src/main/java/com/backendoori/ootw/weather/exception/WeatherControllerAdvice.java
@@ -1,25 +1,29 @@
 package com.backendoori.ootw.weather.exception;
 
 import com.backendoori.ootw.exception.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice("com.backendoori.ootw.weather")
 public class WeatherControllerAdvice {
 
     @ExceptionHandler({IllegalArgumentException.class, IllegalAccessException.class})
-    public ResponseEntity<ErrorResponse> handleExcaption(RuntimeException e) {
+    public ResponseEntity<ErrorResponse> handleIllegalRequestException(RuntimeException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(errorResponse);
     }
 
-    @ExceptionHandler({IllegalStateException.class, Exception.class})
-    public ResponseEntity<ErrorResponse> handleIllegalStateException(Exception e) {
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalStateException(IllegalStateException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        log.error(e.getMessage(), e);
 
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(errorResponse);
