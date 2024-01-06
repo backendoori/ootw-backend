c9cdb9ad541b1ddf2b5598d2fd02d30c048d7746
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-06 19:32:05 +0900

    feat: WeatherControllerAdvice에서 처리하는 예외 수정

--- src/main/java/com/backendoori/ootw/weather/exception/WeatherControllerAdvice.java
+++ src/main/java/com/backendoori/ootw/weather/exception/WeatherControllerAdvice.java
@@ -11,8 +11,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice("com.backendoori.ootw.weather")
 public class WeatherControllerAdvice {
 
-    @ExceptionHandler({IllegalArgumentException.class, IllegalAccessException.class})
-    public ResponseEntity<ErrorResponse> handleIllegalRequestException(RuntimeException e) {
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
