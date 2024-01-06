04c866330cc0627db4ce0b5635d9205a05855422
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-06 18:51:11 +0900

    feat: AvatarItemRequest에 validation을 위한 annotation 추가

--- src/main/java/com/backendoori/ootw/avatar/dto/AvatarItemRequest.java
+++ src/main/java/com/backendoori/ootw/avatar/dto/AvatarItemRequest.java
@@ -1,7 +1,12 @@
 package com.backendoori.ootw.avatar.dto;
 
+import com.backendoori.ootw.common.validation.ItemTypeValid;
+import jakarta.validation.constraints.NotNull;
+
 public record AvatarItemRequest(
+    @ItemTypeValid
     String type,
+    @NotNull
     boolean sex
 ) {
 
