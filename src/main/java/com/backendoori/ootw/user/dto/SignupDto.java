9286e2bf6176f01a5278efcd237d547494cfe05a
ASak1104 <khw56184@gmail.com>
2024-01-08 07:23:14 +0900

    feat: 회원가입 dto에 validation 추가

--- src/main/java/com/backendoori/ootw/user/dto/SignupDto.java
+++ src/main/java/com/backendoori/ootw/user/dto/SignupDto.java
@@ -1,8 +1,23 @@
 package com.backendoori.ootw.user.dto;
 
+import com.backendoori.ootw.user.validation.Message;
+import com.backendoori.ootw.user.validation.Password;
+import com.backendoori.ootw.user.validation.RFC5322;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public record SignupDto(
+    @NotNull
+    @Email(regexp = RFC5322.REGEX)
     String email,
+
+    @NotNull
+    @Password
     String password,
+
+    @NotNull
+    @NotBlank(message = Message.BLANK_NICKNAME)
     String nickname
 ) {
 
