053ab2dc9e9ed30ce7297774060e46764985cf18
ASak1104 <khw56184@gmail.com>
2024-01-08 08:38:55 +0900

    feat: 400 status handler 추가

--- src/main/java/com/backendoori/ootw/exception/GlobalControllerAdvice.java
+++ src/main/java/com/backendoori/ootw/exception/GlobalControllerAdvice.java
@@ -13,6 +13,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(errorResponse);
+    }
+
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ErrorResponse> handleAuthenticationException(AuthenticationException e) {
         ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
