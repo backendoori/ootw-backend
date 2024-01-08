2adc90189338108d0d3e294ce75734cb3c0e28b1
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-08 22:56:32 +0900

    feat: 좋아요 요청과 취소를 위한 api 개발

---
+++ src/main/java/com/backendoori/ootw/like/controller/LikeController.java
@@ -0,0 +1,29 @@
+package com.backendoori.ootw.like.controller;
+
+import com.backendoori.ootw.like.dto.controller.LikeRequest;
+import com.backendoori.ootw.like.dto.controller.LikeResponse;
+import com.backendoori.ootw.like.service.LikeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/likes")
+public class LikeController {
+
+    private final LikeService likeService;
+
+    @PostMapping("")
+    public ResponseEntity<LikeResponse> pushLike(Authentication authentication,
+                                                 @Valid @RequestBody LikeRequest requestDto){
+        Long userId = (Long) authentication.getPrincipal();
+        return ResponseEntity.ok(likeService.requestLike(userId, requestDto.postId()));
+    }
+
+}
