83073855d5dfe77ccd0b8ca3d523680ff0dfb8e4
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-09 16:54:10 +0900

    refactor: WeatherResponse 내부적으로 유효성 검사 로직 구현

--- src/main/java/com/backendoori/ootw/weather/dto/WeatherResponse.java
+++ src/main/java/com/backendoori/ootw/weather/dto/WeatherResponse.java
@@ -10,32 +10,36 @@ import org.springframework.util.Assert;
 
 public record WeatherResponse(
     LocalDateTime currentDateTime,
+    int nx,
+    int ny,
     double currentTemperature,
     String sky,
-    String pty,
-    int nx,
-    int ny
+    String pty
 ) {
 
     public static WeatherResponse from(LocalDateTime dateTime, int nx, int ny,
-                                       Map<ForecastCategory, String> currentWeather) {
+                                       Map<ForecastCategory, String> weatherInfoMap) {
+        checkIncludeCurrentWeather(weatherInfoMap);
+
+        double currentTemperature = Double.parseDouble(weatherInfoMap.get(ForecastCategory.T1H));
+
+        int skyCode = Integer.parseInt(weatherInfoMap.get(ForecastCategory.SKY));
+        String skyType = SkyType.getByCode(skyCode).name();
+
+        int ptyCode = Integer.parseInt(weatherInfoMap.get(ForecastCategory.PTY));
+        String ptyType = PtyType.getByCode(ptyCode).name();
+
+        return new WeatherResponse(dateTime, nx, ny, currentTemperature, skyType, ptyType);
+    }
+
+    private static void checkIncludeCurrentWeather(Map<ForecastCategory, String> currentWeatherMap) {
         Assert.isTrue(
-            currentWeather.containsKey(ForecastCategory.T1H)
-                && currentWeather.containsKey(ForecastCategory.SKY)
-                && currentWeather.containsKey(ForecastCategory.PTY),
+            currentWeatherMap.containsKey(ForecastCategory.T1H)
+                && currentWeatherMap.containsKey(ForecastCategory.SKY)
+                && currentWeatherMap.containsKey(ForecastCategory.PTY),
             () -> {
                 throw ForecastResultErrorManager.getApiServerException();
             });
-
-        double currentTemperature = Double.parseDouble(currentWeather.get(ForecastCategory.T1H));
-
-        int skyCode = Integer.parseInt(currentWeather.get(ForecastCategory.SKY));
-        String sky = SkyType.getByCode(skyCode).name();
-
-        int ptyCode = Integer.parseInt(currentWeather.get(ForecastCategory.PTY));
-        String pty = PtyType.getByCode(ptyCode).name();
-
-        return new WeatherResponse(dateTime, currentTemperature, sky, pty, nx, ny);
     }
 
 }
