21c9fa9054b2278ab12e70d293543811dcd0bf3a
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-09 20:33:40 +0900

    Revert "Merge pull request #19 from Sehee-Lee-01/feat#11-codestyle"
    This reverts commit 4e8c97167d935560aaa1daeef6f940fa9fc806cf, reversing
    changes made to ccb1ab62cf20a22454236bdbaa6c2ffe14a006fe.

--- src/main/java/com/backendoori/ootw/OotwApplication.java
+++ src/main/java/com/backendoori/ootw/OotwApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OotwApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OotwApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OotwApplication.class, args);
+	}
 
 }
