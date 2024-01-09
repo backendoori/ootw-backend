7cc3dfb34e69077db2f9b529061fdff1c504afe5
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-09 17:06:38 +0900

    style: 소스코드 스타일 수정

--- src/main/java/com/backendoori/ootw/exception/ImageUploadException.java
+++ src/main/java/com/backendoori/ootw/exception/ImageUploadException.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
-public class ImageUploadException extends RuntimeException{
+public class ImageUploadException extends RuntimeException {
 
     private final String message = "이미지 업로드 중 예외가 발생했습니다.";
 
