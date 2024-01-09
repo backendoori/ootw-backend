4803b0503091e0294d9c358a0a281d32cc07b4b7
ASak1104 <khw56184@gmail.com>
2024-01-09 18:33:38 +0900

    feat: certificate 객체, repository 추가

---
+++ src/main/java/com/backendoori/ootw/user/repository/CertificateRedisRepository.java
@@ -0,0 +1,11 @@
+package com.backendoori.ootw.user.repository;
+
+import java.util.Optional;
+import com.backendoori.ootw.user.domain.Certificate;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CertificateRedisRepository extends CrudRepository<Certificate, String> {
+
+    Optional<Certificate> findByUserId(Long userId);
+
+}
