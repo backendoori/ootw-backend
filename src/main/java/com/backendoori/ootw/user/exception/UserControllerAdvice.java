3daf6686c01fa83bac4fdd4f708a836f8c7791ed
ASak1104 <khw56184@gmail.com>
2024-01-09 19:06:57 +0900

    feat: 이미 인증된 user에 대한 처리 추가

--- src/main/java/com/backendoori/ootw/user/exception/UserControllerAdvice.java
+++ src/main/java/com/backendoori/ootw/user/exception/UserControllerAdvice.java
@@ -1,13 +1,24 @@
 package com.backendoori.ootw.user.exception;
 
 import com.backendoori.ootw.exception.ErrorResponse;
+import com.backendoori.ootw.user.controller.CertifyEmailController;
+import com.backendoori.ootw.user.controller.UserController;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@RestControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice(basePackageClasses = {UserController.class, CertifyEmailController.class})
 public class UserControllerAdvice {
+
+    @ExceptionHandler(AlreadyCertifiedUserException.class)
+    public ResponseEntity<ErrorResponse> handleAlreadyCertifiedUserException() {
+        return ResponseEntity.status(HttpStatus.NOT_MODIFIED)
+            .build();
+    }
 
     @ExceptionHandler(NonCertifiedUserException.class)
     public ResponseEntity<ErrorResponse> handleNonCertifiedUserException(NonCertifiedUserException e) {
