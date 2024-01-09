d500e057c9637983fd92b75f2de8cdc22c347fc5
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-09 16:53:57 +0900

    refactor: TemperatureArrange 내부적으로 유효성 검사 로직 구현

--- src/main/java/com/backendoori/ootw/weather/domain/TemperatureArrange.java
+++ src/main/java/com/backendoori/ootw/weather/domain/TemperatureArrange.java
@@ -2,7 +2,6 @@ package com.backendoori.ootw.weather.domain;
 
 import java.util.Map;
 import com.backendoori.ootw.weather.domain.forecast.ForecastCategory;
-import com.backendoori.ootw.weather.dto.TemperatureArrangeDto;
 import com.backendoori.ootw.weather.exception.ForecastResultErrorManager;
 import jakarta.persistence.AttributeOverride;
 import jakarta.persistence.Column;
@@ -22,6 +21,8 @@ import org.springframework.util.Assert;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class TemperatureArrange {
 
+    private static final String TEMPERATURE_ARRANGE_EXCEPTION = "기상청에서 제공한 일 최저 기온, 일 최고 기온 값이 유효하지 않습니다.";
+
     @Embedded
     @AttributeOverride(name = "value", column = @Column(name = "min_temperature", nullable = false))
     private Temperature min;
@@ -30,32 +31,32 @@ public class TemperatureArrange {
     @AttributeOverride(name = "value", column = @Column(name = "max_temperature", nullable = false))
     private Temperature max;
 
-    // TODO: dto에서 생성 vs  vs ModelMapper 사용
-    public static TemperatureArrange from(TemperatureArrangeDto weatherDto) {
-        Temperature minTemperature = Temperature.of(weatherDto.min());
-        Temperature maxTemperature = Temperature.of(weatherDto.max());
-        validateArrange(minTemperature, maxTemperature);
+    public static TemperatureArrange from(Map<ForecastCategory, String> weatherInfoMap) {
+        checkIncludeTemperatureArrange(weatherInfoMap);
 
-        return new TemperatureArrange(minTemperature, maxTemperature);
+        Temperature dayMinTemperature =
+            Temperature.of(Double.parseDouble(weatherInfoMap.get(ForecastCategory.TMN)));
+        Temperature dayMaxTemperature =
+            Temperature.of(Double.parseDouble(weatherInfoMap.get(ForecastCategory.TMN)));
+
+        validateArrange(dayMinTemperature, dayMaxTemperature);
+
+        return new TemperatureArrange(dayMinTemperature, dayMaxTemperature);
     }
 
-    public static TemperatureArrange from(Map<ForecastCategory, String> temperatureArrangeMap) {
+    private static void validateArrange(Temperature dayMinTemperature, Temperature dayMaxTemperature) {
+        Assert.isTrue(dayMinTemperature.getValue() <= dayMaxTemperature.getValue(), () -> {
+            throw new IllegalStateException(TEMPERATURE_ARRANGE_EXCEPTION);
+        });
+    }
+
+    private static void checkIncludeTemperatureArrange(Map<ForecastCategory, String> temperatureArrangeMap) {
         Assert.isTrue(
             temperatureArrangeMap.containsKey(ForecastCategory.TMN)
                 && temperatureArrangeMap.containsKey(ForecastCategory.TMX),
             () -> {
                 throw ForecastResultErrorManager.getApiServerException();
             });
-
-        Temperature dayMinTemperature =
-            Temperature.of(Double.parseDouble(temperatureArrangeMap.get(ForecastCategory.TMN)));
-        Temperature dayMaxTemperature =
-            Temperature.of(Double.parseDouble(temperatureArrangeMap.get(ForecastCategory.TMX)));
-
-        return new TemperatureArrange(dayMinTemperature, dayMaxTemperature);
     }
 
-    private static void validateArrange(Temperature dayMinTemperature, Temperature dayMaxTemperature) {
-        Assert.isTrue(dayMinTemperature.getValue() <= dayMaxTemperature.getValue(), "일 최저 기온, 일 최고 기온 값이 적절하지 않습니다.");
-    }
 }
