43330b1634cc923c7b440b554860225f5459a198
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-09 16:56:31 +0900

    style: 불필요한 유효성 검사 로직 삭제 - 이미 검증되어 생성된 TemperatureArrange로부터 생성하는 것이 대부분 - 보내는 용도이기 때문에 요청으로부터 받는 것보다 검증을 덜 신경써도 될 것 같다.

--- src/main/java/com/backendoori/ootw/weather/dto/TemperatureArrangeDto.java
+++ src/main/java/com/backendoori/ootw/weather/dto/TemperatureArrangeDto.java
@@ -1,21 +1,10 @@
 package com.backendoori.ootw.weather.dto;
 
 import com.backendoori.ootw.weather.domain.TemperatureArrange;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 public record TemperatureArrangeDto(
-
-    @NotNull
-    @Min(value = -900)
-    @Max(value = 900)
-    Double min,
-
-    @NotNull
-    @Min(value = -900)
-    @Max(value = 900)
-    Double max
+    double min,
+    double max
 ) {
 
     public static TemperatureArrangeDto from(TemperatureArrange weather) {
