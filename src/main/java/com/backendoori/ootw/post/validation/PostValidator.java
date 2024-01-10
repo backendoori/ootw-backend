687d5d6d6e1e355f6ca5f79651dbd83a831aba2c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-11 07:20:33 +0900

    Revert "refactor: AssertUtil 업그레이드 및 적용"
    This reverts commit 0a01a2353d3d50852d989f49ce419207ddf1b029.

--- src/main/java/com/backendoori/ootw/post/validation/PostValidator.java
+++ src/main/java/com/backendoori/ootw/post/validation/PostValidator.java
@@ -1,5 +1,6 @@
 package com.backendoori.ootw.post.validation;
 
+import static com.backendoori.ootw.post.validation.Message.BLANK_POST_CONTENT;
 import static com.backendoori.ootw.post.validation.Message.BLANK_POST_TITLE;
 import static com.backendoori.ootw.post.validation.Message.INVALID_POST_CONTENT;
 import static com.backendoori.ootw.post.validation.Message.INVALID_POST_TITLE;
@@ -7,10 +8,10 @@ import static com.backendoori.ootw.post.validation.Message.NULL_POST;
 import static com.backendoori.ootw.post.validation.Message.NULL_TEMPERATURE_ARRANGE;
 import static com.backendoori.ootw.post.validation.Message.NULL_WRITER;
 
-import com.backendoori.ootw.common.AssertUtil;
 import com.backendoori.ootw.post.dto.PostSaveRequest;
 import com.backendoori.ootw.user.domain.User;
 import com.backendoori.ootw.weather.domain.TemperatureArrange;
+import org.springframework.util.Assert;
 
 public class PostValidator {
 
@@ -18,27 +19,29 @@ public class PostValidator {
     private static final Integer MAX_CONTENT_LENGTH = 500;
 
     public static void validateUser(User user) {
-        AssertUtil.notNull(user, NULL_WRITER);
+        Assert.notNull(user, NULL_WRITER);
     }
 
     public static void validatePostSaveRequest(PostSaveRequest request) {
-        AssertUtil.notNull(request, NULL_POST);
+        Assert.notNull(request, NULL_POST);
         validateTitle(request.title());
         validateContent(request.content());
     }
 
     public static void validateTemperatureArrange(TemperatureArrange temperatureArrange) {
-        AssertUtil.notNull(temperatureArrange, NULL_TEMPERATURE_ARRANGE);
+        Assert.notNull(temperatureArrange, NULL_TEMPERATURE_ARRANGE);
     }
 
     private static void validateTitle(String title) {
-        AssertUtil.notBlank(title, BLANK_POST_TITLE);
-        AssertUtil.isFalse(title.length() > MAX_TITLE_LENGTH, INVALID_POST_TITLE);
+        Assert.notNull(title, BLANK_POST_TITLE);
+        Assert.isTrue(!title.isBlank(), BLANK_POST_TITLE);
+        Assert.isTrue(!(title.length() > MAX_TITLE_LENGTH), INVALID_POST_TITLE);
     }
 
     private static void validateContent(String content) {
-        AssertUtil.notBlank(content, BLANK_POST_TITLE);
-        AssertUtil.isFalse(content.length() > MAX_CONTENT_LENGTH, INVALID_POST_CONTENT);
+        Assert.notNull(content, BLANK_POST_CONTENT);
+        Assert.isTrue(!content.isBlank(), BLANK_POST_CONTENT);
+        Assert.isTrue(!(content.length() > MAX_CONTENT_LENGTH), INVALID_POST_CONTENT);
     }
 
 }
