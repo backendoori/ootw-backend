3016f033b6aefe0ed398f7a3bafeafbf33bc0873
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-10 23:40:24 +0900

    test: ForecastApiClientTest 리팩토링 및 파싱 불가능한 경우 추가

--- src/test/java/com/backendoori/ootw/weather/util/client/ForecastApiClientTest.java
+++ src/test/java/com/backendoori/ootw/weather/util/client/ForecastApiClientTest.java
@@ -1,20 +1,29 @@
 package com.backendoori.ootw.weather.util.client;
 
-import static com.backendoori.ootw.weather.util.client.ForecastApiSourceProvider.INVALID_PARAMETER_FORECAST_RESPONSE;
-import static com.backendoori.ootw.weather.util.client.ForecastApiSourceProvider.NO_DATA_FORECAST_RESPONSE;
-import static com.backendoori.ootw.weather.util.client.ForecastApiSourceProvider.TEMP_BASE_DATETIME;
-import static com.backendoori.ootw.weather.util.client.ForecastApiSourceProvider.VALID_NX;
-import static com.backendoori.ootw.weather.util.client.ForecastApiSourceProvider.VALID_NY;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonRequestSourceProvider.ULTRA_SHORT_FORECAST_BASE_DATETIME;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonRequestSourceProvider.VALID_NX;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonRequestSourceProvider.VALID_NY;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonRequestSourceProvider.VILLAGE_FORECAST_BASE_DATETIME;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonResponseSourceProvider.FORECAST_RESPONSE_WITH_ONE_ITEM;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonResponseSourceProvider.INVALID_FORECAST_RESPONSE;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonResponseSourceProvider.INVALID_PARAMETER_FORECAST_RESPONSE;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonResponseSourceProvider.NO_DATA_FORECAST_RESPONSE;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonResponseSourceProvider.ULTRA_SHORT_FORECAST_RESPONSE_WITH_ONE_ITEM_RESPONSE;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonResponseSourceProvider.VILLAGE_FORECAST_RESPONSE_WITH_ONE_ITEM_RESPONSE;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Stream;
+import com.backendoori.ootw.weather.util.deserializer.ForecastResultItem;
 import net.datafaker.Faker;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -34,77 +43,168 @@ class ForecastApiClientTest {
     @Autowired
     ForecastApiClient forecastApiClient;
 
-    static Stream<Arguments> provideInvalidRange() {
-        return Stream.of(
-            Arguments.of(FAKER.number().negative(), VALID_NY),
-            Arguments.of(VALID_NX, FAKER.number().negative()),
-            Arguments.of(FAKER.number().numberBetween(1000, 10000), VALID_NY),
-            Arguments.of(VALID_NX, FAKER.number().numberBetween(1000, 10000)));
+    @Nested
+    @DisplayName("현재 초단기예보 불러오기")
+    class RequestUltraShortForecastItems {
+
+        static Stream<Arguments> provideInvalidLocation() {
+            return Stream.of(
+                Arguments.of(VALID_NX, FAKER.number().negative()),
+                Arguments.of(FAKER.number().negative(), VALID_NY),
+                Arguments.of(VALID_NX, FAKER.number().numberBetween(1000, 10000)),
+                Arguments.of(FAKER.number().numberBetween(1000, 10000), VALID_NY)
+            );
+        }
+
+        void givenUltraShortForecastApiResponse(int nx, int ny, String response) {
+            given(forecastApi.getUltraShortForecast(
+                anyString(),
+                anyInt(),
+                anyInt(),
+                anyString(),
+                eq(ULTRA_SHORT_FORECAST_BASE_DATETIME.baseDate()),
+                eq(ULTRA_SHORT_FORECAST_BASE_DATETIME.baseTime()),
+                eq(nx),
+                eq(ny)))
+                .willReturn(response);
+        }
+
+        @Test
+        @DisplayName("초단기예보를 불러오고 응답 파싱에 성공한다.")
+        void requestUltraShortForecastItemsSuccess() {
+            // given
+            givenUltraShortForecastApiResponse(VALID_NX, VALID_NY,
+                ULTRA_SHORT_FORECAST_RESPONSE_WITH_ONE_ITEM_RESPONSE);
+
+            // when
+            List<ForecastResultItem> resultItems = forecastApiClient
+                .requestUltraShortForecastItems(ULTRA_SHORT_FORECAST_BASE_DATETIME, VALID_NX, VALID_NY);
+
+            // then
+            assertThat(resultItems).contains(FORECAST_RESPONSE_WITH_ONE_ITEM);
+        }
+
+        @Test
+        @DisplayName("사용할 수 없는 응답이 오는 경우, 초단기예보를 불러오고 응답 파싱에 실패한다.")
+        void requestUltraShortForecastItemsFail() {
+            // given
+            givenUltraShortForecastApiResponse(VALID_NX, VALID_NY, INVALID_FORECAST_RESPONSE);
+
+            // when // then
+            assertThrows(IllegalStateException.class,
+                () -> forecastApiClient.requestUltraShortForecastItems(ULTRA_SHORT_FORECAST_BASE_DATETIME, VALID_NX,
+                    VALID_NY));
+        }
+
+        @Test
+        @DisplayName("정보가 없는 위치 값으로 현재 초단기예보 불러오기에 실패한다.")
+        void requestUltraShortForecastItemsFailByNoSuchElementException() {
+            // given
+            int nx = 0;
+            int ny = 0;
+
+            givenUltraShortForecastApiResponse(nx, ny, NO_DATA_FORECAST_RESPONSE);
+
+            // when // then
+            assertThrows(NoSuchElementException.class,
+                () -> forecastApiClient.requestUltraShortForecastItems(ULTRA_SHORT_FORECAST_BASE_DATETIME, nx, ny));
+        }
+
+        @ParameterizedTest
+        @MethodSource("provideInvalidLocation")
+        @DisplayName("유효하지 않은 파라미터 범위로 현재 초단기예보 불러오기에 실패한다.")
+        void requestUltraShortForecastItemsFailByIllegalRange(int invalidNx, int invalidNy) {
+            // given
+            givenUltraShortForecastApiResponse(invalidNx, invalidNy, INVALID_PARAMETER_FORECAST_RESPONSE);
+
+            // when // then
+            assertThrows(IllegalArgumentException.class,
+                () -> forecastApiClient.requestUltraShortForecastItems(ULTRA_SHORT_FORECAST_BASE_DATETIME, invalidNx,
+                    invalidNy));
+        }
+
     }
 
-    @Test
-    @DisplayName("정보가 없는 위치 값으로 현재 초단기예보 불러오기에 실패한다.")
-    void requestUltraShortForecastItemsFailByNoSuchElementException() {
-        // given
-        int nx = 0;
-        int ny = 0;
+    @Nested
+    @DisplayName("현재 단기예보 불러오기")
+    class RequestVillageForecastItems {
 
-        givenForecastApiResponse(nx, ny, NO_DATA_FORECAST_RESPONSE);
+        static Stream<Arguments> provideInvalidLocation() {
+            return Stream.of(
+                Arguments.of(VALID_NX, FAKER.number().negative()),
+                Arguments.of(FAKER.number().negative(), VALID_NY),
+                Arguments.of(VALID_NX, FAKER.number().numberBetween(1000, 10000)),
+                Arguments.of(FAKER.number().numberBetween(1000, 10000), VALID_NY)
+            );
+        }
 
-        // when // then
-        assertThrows(NoSuchElementException.class,
-            () -> forecastApiClient.requestUltraShortForecastItems(TEMP_BASE_DATETIME, nx, ny));
-    }
+        void givenVillageForecastApiResponse(int nx, int ny, String response) {
+            given(forecastApi.getVillageForecast(
+                anyString(),
+                anyInt(),
+                anyInt(),
+                anyString(),
+                eq(VILLAGE_FORECAST_BASE_DATETIME.baseDate()),
+                eq(VILLAGE_FORECAST_BASE_DATETIME.baseTime()),
+                eq(nx),
+                eq(ny)))
+                .willReturn(response);
+        }
 
-    @ParameterizedTest
-    @MethodSource("provideInvalidRange")
-    @DisplayName("유효하지 않은 파라미터 범위로 현재 초단기예보 불러오기에 실패한다.")
-    void requestUltraShortForecastItemsFailByIllegalRange(Integer nx, Integer ny) {
-        // given
-        givenForecastApiResponse(nx, ny, INVALID_PARAMETER_FORECAST_RESPONSE);
+        @Test
+        @DisplayName("단기예보를 불러오고 응답 파싱에 성공한다.")
+        void requestVillageForecastItemsSuccess() {
+            // given
+            givenVillageForecastApiResponse(VALID_NX, VALID_NY, VILLAGE_FORECAST_RESPONSE_WITH_ONE_ITEM_RESPONSE);
 
-        // when // then
-        assertThrows(IllegalArgumentException.class,
-            () -> forecastApiClient.requestUltraShortForecastItems(TEMP_BASE_DATETIME, nx, ny));
-    }
+            // when
+            List<ForecastResultItem> resultItems = forecastApiClient
+                .requestVillageForecastItems(VILLAGE_FORECAST_BASE_DATETIME, VALID_NX, VALID_NY);
 
-    @Test
-    @DisplayName("정보가 없는 위치 값으로 현재 단기예보 불러오기에 실패한다.")
-    void requestVillageForecastItemsFailByNoSuchElementException() {
-        // given
-        int nx = 0;
-        int ny = 0;
+            // then
+            assertThat(resultItems).contains(FORECAST_RESPONSE_WITH_ONE_ITEM);
 
-        givenForecastApiResponse(nx, ny, NO_DATA_FORECAST_RESPONSE);
+        }
 
-        // when // then
-        assertThrows(NoSuchElementException.class,
-            () -> forecastApiClient.requestUltraShortForecastItems(TEMP_BASE_DATETIME, nx, ny));
-    }
+        @Test
+        @DisplayName("사용할 수 없는 응답이 오는 경우, 단기예보를 불러오고 응답 파싱에 실패한다.")
+        void requestVillageForecastItemsFail() {
+            // given
+            givenVillageForecastApiResponse(VALID_NX, VALID_NY, INVALID_FORECAST_RESPONSE);
 
-    @ParameterizedTest
-    @MethodSource("provideInvalidRange")
-    @DisplayName("유효하지 않은 파라미터 범위로 현재 단기예보 불러오기에 실패한다.")
-    void requestVillageForecastItemsFailByIllegalRange(Integer nx, Integer ny) {
-        // given
-        givenForecastApiResponse(nx, ny, INVALID_PARAMETER_FORECAST_RESPONSE);
+            // when // then
+            assertThrows(IllegalStateException.class,
+                () -> forecastApiClient.requestVillageForecastItems(VILLAGE_FORECAST_BASE_DATETIME, VALID_NX,
+                    VALID_NY));
+        }
 
-        // when // then
-        assertThrows(IllegalArgumentException.class,
-            () -> forecastApiClient.requestVillageForecastItems(TEMP_BASE_DATETIME, nx, ny));
-    }
+        @Test
+        @DisplayName("정보가 없는 위치 값으로 현재 단기예보 불러오기에 실패한다.")
+        void requestVillageForecastItemsFailByNoSuchElementException() {
+            // given
+            int nx = 0;
+            int ny = 0;
 
-    void givenForecastApiResponse(int nx, int ny, String response) {
-        given(forecastApi.getUltraShortForecast(
-            anyString(),
-            anyInt(),
-            anyInt(),
-            anyString(),
-            eq(TEMP_BASE_DATETIME.baseDate()),
-            eq(TEMP_BASE_DATETIME.baseTime()),
-            eq(nx),
-            eq(ny)))
-            .willReturn(response);
+            givenVillageForecastApiResponse(nx, ny, NO_DATA_FORECAST_RESPONSE);
+
+            // when // then
+            assertThrows(NoSuchElementException.class,
+                () -> forecastApiClient.requestVillageForecastItems(VILLAGE_FORECAST_BASE_DATETIME, nx, ny));
+        }
+
+        @ParameterizedTest
+        @MethodSource("provideInvalidLocation")
+        @DisplayName("유효하지 않은 파라미터 범위로 현재 단기예보 불러오기에 실패한다.")
+        void requestVillageForecastItemsFailByIllegalRange(int invalidNx, int invalidNy) {
+            // given
+            givenVillageForecastApiResponse(invalidNx, invalidNy, INVALID_PARAMETER_FORECAST_RESPONSE);
+
+            // when // then
+            assertThrows(IllegalArgumentException.class,
+                () -> forecastApiClient.requestVillageForecastItems(ULTRA_SHORT_FORECAST_BASE_DATETIME, invalidNx,
+                    invalidNy));
+        }
+
     }
 
 }
