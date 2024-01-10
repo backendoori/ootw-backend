d6e9d63d0754db764da970a8b4e9668c3bdde824
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-01-11 01:41:56 +0900

    feat: login 한 사용자와 login 안한 사용자의 좋아요 누른 여부 다르게 반영하는 로직

--- src/main/java/com/backendoori/ootw/post/service/PostService.java
+++ src/main/java/com/backendoori/ootw/post/service/PostService.java
@@ -2,8 +2,11 @@ package com.backendoori.ootw.post.service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import com.backendoori.ootw.common.image.ImageService;
 import com.backendoori.ootw.exception.UserNotFoundException;
+import com.backendoori.ootw.like.domain.Like;
+import com.backendoori.ootw.like.repository.LikeRepository;
 import com.backendoori.ootw.post.domain.Post;
 import com.backendoori.ootw.post.dto.PostReadResponse;
 import com.backendoori.ootw.post.dto.PostSaveRequest;
@@ -23,10 +26,12 @@ import org.springframework.web.multipart.MultipartFile;
 @Service
 public class PostService {
 
+    private static final String ANONYMOUS_USER_PRINCIPLE = "anonymousUser";
     private final PostRepository postRepository;
     private final UserRepository userRepository;
     private final ImageService imageService;
     private final WeatherService weatherService;
+    private final LikeRepository likeRepository;
 
     @Transactional
     public PostSaveResponse save(PostSaveRequest request, MultipartFile postImg) {
@@ -44,15 +49,50 @@ public class PostService {
     public PostReadResponse getDetailByPostId(Long postId) {
         Post post = postRepository.findByIdWithUserEntityGraph(postId)
             .orElseThrow(() -> new NoSuchElementException("해당하는 게시글이 없습니다."));
+        PostReadResponse response = PostReadResponse.from(post);
 
-        return PostReadResponse.from(post);
+        if (!isLogin()) {
+            return response;
+        }
+
+        Optional<Like> like = likeRepository.findByUserIdAndPost(getUserId(), post);
+        like.ifPresent(
+            existLike -> {
+                if (existLike.getIsLike()) {
+                    response.updateIsLike();
+                }
+            }
+        );
+
+        return response;
     }
 
     @Transactional(readOnly = true)
     public List<PostReadResponse> getAll() {
-        return postRepository.findAllWithUserEntityGraph()
+        List<PostReadResponse> postResponseList = postRepository.findAllWithUserEntityGraph()
             .stream()
             .map(PostReadResponse::from)
+            .toList();
+        if (!isLogin()) {
+            return postResponseList;
+        }
+
+        List<Long> likes = getLikedPostId(getUserId());
+        if (likes.isEmpty()) {
+            return postResponseList;
+        }
+
+        return postResponseList.stream()
+            .peek(post -> {
+                if (likes.contains(post.getPostId())) {
+                    post.updateIsLike();
+                }
+            }).toList();
+    }
+
+    private List<Long> getLikedPostId(long userId) {
+        return likeRepository.findByUserAndIsLike(userId, true)
+            .stream().map(like -> like.getPost().getId())
             .toList();
     }
 
@@ -61,6 +101,14 @@ public class PostService {
             .getContext()
             .getAuthentication()
             .getPrincipal();
+    }
+
+    //TODO: 이 부분을 .equals 써야하는지 궁금하다.
+    private boolean isLogin() {
+        return SecurityContextHolder
+            .getContext()
+            .getAuthentication()
+            .getPrincipal() != ANONYMOUS_USER_PRINCIPLE;
     }
 
 }
