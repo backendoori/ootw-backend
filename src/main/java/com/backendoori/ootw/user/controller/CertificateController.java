e7a2080b884b28eea086d7eadbbe569118406fdd
ASak1104 <khw56184@gmail.com>
2024-01-10 19:11:22 +0900

    fix: certificate http method 수정

--- src/main/java/com/backendoori/ootw/user/controller/CertificateController.java
+++ src/main/java/com/backendoori/ootw/user/controller/CertificateController.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +22,7 @@ public class CertificateController {
 
     private final CertificateService certificateService;
 
-    @PutMapping("/certificate")
+    @PatchMapping("/certificate")
     public ResponseEntity<Void> sendCertificate(
         @NotNull
         @NotBlank
