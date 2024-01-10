763b68e4f9c8eff0642d91ee4a44c26c8db53ff2
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-11 07:02:26 +0900

    refactor: 예외 메시지 명시

--- src/main/java/com/backendoori/ootw/post/dto/PostSaveRequest.java
+++ src/main/java/com/backendoori/ootw/post/dto/PostSaveRequest.java
@@ -2,6 +2,8 @@ package com.backendoori.ootw.post.dto;
 
 import static com.backendoori.ootw.post.validation.Message.BLANK_POST_CONTENT;
 import static com.backendoori.ootw.post.validation.Message.BLANK_POST_TITLE;
+import static com.backendoori.ootw.post.validation.Message.INVALID_POST_CONTENT;
+import static com.backendoori.ootw.post.validation.Message.INVALID_POST_TITLE;
 
 import com.backendoori.ootw.weather.domain.Coordinate;
 import com.backendoori.ootw.weather.validation.Grid;
@@ -10,11 +12,11 @@ import jakarta.validation.constraints.Size;
 
 public record PostSaveRequest(
     @NotBlank(message = BLANK_POST_TITLE)
-    @Size(max = 30)
+    @Size(max = 30, message = INVALID_POST_TITLE)
     String title,
 
     @NotBlank(message = BLANK_POST_CONTENT)
-    @Size(max = 500)
+    @Size(max = 500, message = INVALID_POST_CONTENT)
     String content,
 
     @Grid
