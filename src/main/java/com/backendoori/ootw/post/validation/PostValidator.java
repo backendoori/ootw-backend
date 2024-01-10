b542e6281e81962eed5c9d98163b3c40d87ae899
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-11 07:39:13 +0900

    fix: AssertUtil 적용 후 잘못된 로직 수정

--- src/main/java/com/backendoori/ootw/post/validation/PostValidator.java
+++ src/main/java/com/backendoori/ootw/post/validation/PostValidator.java
@@ -8,10 +8,10 @@ import static com.backendoori.ootw.post.validation.Message.NULL_POST;
 import static com.backendoori.ootw.post.validation.Message.NULL_TEMPERATURE_ARRANGE;
 import static com.backendoori.ootw.post.validation.Message.NULL_WRITER;
 
+import com.backendoori.ootw.common.AssertUtil;
 import com.backendoori.ootw.post.dto.PostSaveRequest;
 import com.backendoori.ootw.user.domain.User;
 import com.backendoori.ootw.weather.domain.TemperatureArrange;
-import org.springframework.util.Assert;
 
 public class PostValidator {
 
@@ -19,29 +19,27 @@ public class PostValidator {
     private static final Integer MAX_CONTENT_LENGTH = 500;
 
     public static void validateUser(User user) {
-        Assert.notNull(user, NULL_WRITER);
+        AssertUtil.notNull(user, NULL_WRITER);
     }
 
     public static void validatePostSaveRequest(PostSaveRequest request) {
-        Assert.notNull(request, NULL_POST);
+        AssertUtil.notNull(request, NULL_POST);
         validateTitle(request.title());
         validateContent(request.content());
     }
 
     public static void validateTemperatureArrange(TemperatureArrange temperatureArrange) {
-        Assert.notNull(temperatureArrange, NULL_TEMPERATURE_ARRANGE);
+        AssertUtil.notNull(temperatureArrange, NULL_TEMPERATURE_ARRANGE);
     }
 
     private static void validateTitle(String title) {
-        Assert.notNull(title, BLANK_POST_TITLE);
-        Assert.isTrue(!title.isBlank(), BLANK_POST_TITLE);
-        Assert.isTrue(!(title.length() > MAX_TITLE_LENGTH), INVALID_POST_TITLE);
+        AssertUtil.notBlank(title, BLANK_POST_TITLE);
+        AssertUtil.isTrue(title.length() > MAX_TITLE_LENGTH, INVALID_POST_TITLE);
     }
 
     private static void validateContent(String content) {
-        Assert.notNull(content, BLANK_POST_CONTENT);
-        Assert.isTrue(!content.isBlank(), BLANK_POST_CONTENT);
-        Assert.isTrue(!(content.length() > MAX_CONTENT_LENGTH), INVALID_POST_CONTENT);
+        AssertUtil.notBlank(content, BLANK_POST_CONTENT);
+        AssertUtil.isTrue(content.length() > MAX_CONTENT_LENGTH, INVALID_POST_CONTENT);
     }
 
 }
