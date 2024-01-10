a6be4a575545ab73906f32219d222c233fcc3e5e
ASak1104 <khw56184@gmail.com>
2024-01-10 15:16:15 +0900

    fix: certify request를 patch method로 변경

--- src/main/java/com/backendoori/ootw/user/controller/CertificateController.java
+++ src/main/java/com/backendoori/ootw/user/controller/CertificateController.java
@@ -5,7 +5,7 @@ import com.backendoori.ootw.user.service.CertificateService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,7 @@ public class CertificateController {
 
     private final CertificateService certificateService;
 
-    @PostMapping("/certify")
+    @PatchMapping("/certify")
     public ResponseEntity<Void> certify(CertifyDto certifyDto) {
         certificateService.certify(certifyDto);
 
