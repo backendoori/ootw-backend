1d105516d5c4249f728d07c30ae5095b356885df
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-11 07:38:44 +0900

    refactor: 예외 메시지 수정

--- src/main/java/com/backendoori/ootw/post/validation/Message.java
+++ src/main/java/com/backendoori/ootw/post/validation/Message.java
@@ -11,7 +11,7 @@ public final class Message {
 
     public static final String NULL_WRITER = "게시글 생성 요청 사용자가 null이어서는 안됩니다.";
 
-    public static final String NULL_TEMPERATURE_ARRANGE = "게시글 기온 범위가 null이어서는 안됩니다.";
+    public static final String NULL_TEMPERATURE_ARRANGE = "기상청 API에서 기온 범위 값을 null로 반환했습니다.";
 
     public static final String BLANK_POST_TITLE = "게시글 제목이 null이거나 공백이어서는 안됩니다.";
     public static final String INVALID_POST_TITLE = "게시글 제목은 30자 이내여야 합니다.";
