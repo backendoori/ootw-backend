242ce4b6561db3ee80a43dce5f6dad62cd83c0fd
ASak1104 <khw56184@gmail.com>
2024-01-10 11:32:19 +0900

    test: 메일 전송 테스트 개선

--- src/test/java/com/backendoori/ootw/common/OotwMailSenderTest.java
+++ src/test/java/com/backendoori/ootw/common/OotwMailSenderTest.java
@@ -1,40 +1,41 @@
 package com.backendoori.ootw.common;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThat;
 
+import com.icegreen.greenmail.util.GreenMailUtil;
 import jakarta.mail.MessagingException;
-import net.datafaker.Faker;
-import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import jakarta.mail.internet.MimeMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OotwMailSenderTest {
-
-    static final Faker faker = new Faker();
+class OotwMailSenderTest extends MailTest {
 
     @Autowired
     OotwMailSender ootwMailSender;
-
-    @Value("${spring.mail.username}")
-    String sender;
 
     @DisplayName("메일 전송에 성공한다")
     @Test
     void testSendMail() throws MessagingException {
         // given
-        String receiver = sender;
-        String title = faker.book().title();
-        String body = faker.emoji().smiley();
+        String title = GreenMailUtil.random();
+        String body = GreenMailUtil.random();
 
         // when
-        ThrowingCallable sendMail = () -> ootwMailSender.sendMail(receiver, title, body);
+        ootwMailSender.sendMail(SMTP_ADDRESS, title, body);
 
         // then
-        assertThatNoException().isThrownBy(sendMail);
+        smtp.waitForIncomingEmail(30 * 1000L, 1);
+
+        MimeMessage[] receivedMessages = smtp.getReceivedMessages();
+        MimeMessage message = receivedMessages[0];
+        String actualReceiver = message.getAllRecipients()[0].toString();
+
+        assertThat(receivedMessages).hasSize(1);
+        assertThat(actualReceiver).isEqualTo(SMTP_ADDRESS);
+        assertThat(GreenMailUtil.getBody(message)).isEqualTo(body);
     }
 
 }
