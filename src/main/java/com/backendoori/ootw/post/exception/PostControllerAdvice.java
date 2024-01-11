af48f9b588ef1263474e89e8f4c3f0be1148dd3c
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-11 16:23:42 +0900

    feat: PostControllerAdvice 구현

---
+++ src/main/java/com/backendoori/ootw/post/exception/PostControllerAdvice.java
@@ -0,0 +1,24 @@
+package com.backendoori.ootw.post.exception;
+
+import com.backendoori.ootw.exception.ErrorResponse;
+import com.backendoori.ootw.post.controller.PostController;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Order(Ordered.HIGHEST_PRECEDENCE)
+@RestControllerAdvice(basePackageClasses = PostController.class)
+public class PostControllerAdvice {
+
+    @ExceptionHandler(NoPostPermissionException.class)
+    public ResponseEntity<ErrorResponse> handleNoPostPermissionException(NoPostPermissionException e) {
+        ErrorResponse errorResponse = new ErrorResponse(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
+            .body(errorResponse);
+    }
+
+}
