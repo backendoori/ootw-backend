a22fa61116a5a5b8fc9952b13a280274caf678f5
LEE JUNGWON <85065626+shoeone96@users.noreply.github.com>
2024-01-11 19:55:13 +0900

    feat: avatarItem 내보는 클래스 수정

--- src/main/java/com/backendoori/ootw/avatar/dto/AvatarItemResponse.java
+++ src/main/java/com/backendoori/ootw/avatar/dto/AvatarItemResponse.java
@@ -3,13 +3,16 @@ package com.backendoori.ootw.avatar.dto;
 import com.backendoori.ootw.avatar.domain.AvatarItem;
 
 public record AvatarItemResponse(
+    Long avatarItemId,
     String type,
     String sex,
     String url
 ) {
 
     public static AvatarItemResponse from(AvatarItem avatarItem) {
-        return new AvatarItemResponse(avatarItem.getItemType().name(),
+        return new AvatarItemResponse(
+            avatarItem.getId(),
+            avatarItem.getItemType().name(),
             avatarItem.getSex().name(),
             avatarItem.getImage());
     }
