d9e6c59c636d36f9ddf9d46e0f1e237465703af9
ASak1104 <khw56184@gmail.com>
2024-01-11 18:01:16 +0900

    refactor: 필요없는 validate annotation 제거

--- src/main/java/com/backendoori/ootw/user/dto/CertifyDto.java
+++ src/main/java/com/backendoori/ootw/user/dto/CertifyDto.java
@@ -4,18 +4,15 @@ import com.backendoori.ootw.user.domain.Certificate;
 import com.backendoori.ootw.user.validation.RFC5322;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record CertifyDto(
-    @NotNull
     @NotBlank
     @Size(max = 255)
     @Email(regexp = RFC5322.REGEX)
     String email,
 
-    @NotNull
     @NotBlank
     @Pattern(regexp = Certificate.REGEX)
     String code
