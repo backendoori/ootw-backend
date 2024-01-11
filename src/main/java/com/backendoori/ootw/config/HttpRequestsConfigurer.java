1b7ec1c7fe0beb3a9f71f5867129424adee1b299
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-11 16:21:47 +0900

    fix: 게시글 조회 권한 설정

--- src/main/java/com/backendoori/ootw/config/HttpRequestsConfigurer.java
+++ src/main/java/com/backendoori/ootw/config/HttpRequestsConfigurer.java
@@ -1,5 +1,8 @@
 package com.backendoori.ootw.config;
 
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
@@ -10,12 +13,15 @@ public class HttpRequestsConfigurer
     implements Customizer<AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry> {
 
     private static final String AUTH_RESOURCE = "/api/v1/auth/**";
+    private static final String POST_RESOURCE = "/api/v1/posts/**";
 
     @Override
     public void customize(
         AuthorizeHttpRequestsConfigurer<HttpSecurity>.AuthorizationManagerRequestMatcherRegistry authorizeRequests) {
         authorizeRequests
             .requestMatchers(AUTH_RESOURCE)
+            .permitAll()
+            .requestMatchers(antMatcher(HttpMethod.GET, POST_RESOURCE))
             .permitAll()
             .anyRequest()
             .authenticated();
