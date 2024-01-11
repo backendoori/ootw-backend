479b6a192e495a437d783a513ac45d5b6bca3a55
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-11 16:23:54 +0900

    feat: PostUpdateRequest 구현

---
+++ src/main/java/com/backendoori/ootw/post/dto/request/PostUpdateRequest.java
@@ -0,0 +1,21 @@
+package com.backendoori.ootw.post.dto.request;
+
+import static com.backendoori.ootw.post.validation.Message.BLANK_POST_CONTENT;
+import static com.backendoori.ootw.post.validation.Message.BLANK_POST_TITLE;
+import static com.backendoori.ootw.post.validation.Message.INVALID_POST_CONTENT;
+import static com.backendoori.ootw.post.validation.Message.INVALID_POST_TITLE;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostUpdateRequest(
+    @NotBlank(message = BLANK_POST_TITLE)
+    @Size(max = 30, message = INVALID_POST_TITLE)
+    String title,
+
+    @NotBlank(message = BLANK_POST_CONTENT)
+    @Size(max = 500, message = INVALID_POST_CONTENT)
+    String content
+) {
+
+}
