f0546f48d2e3dfe2dbb159a3921dac2368b242ea
Sehee-Lee-01 <tpfktpgml24@kookmin.ac.kr>
2024-01-12 23:51:27 +0900

    refactor: 인증보다는 인가 예외라서 우선 런타임 예외 상속으로 변경

--- src/main/java/com/backendoori/ootw/exception/PermissionException.java
+++ src/main/java/com/backendoori/ootw/exception/PermissionException.java
@@ -1,8 +1,6 @@
 package com.backendoori.ootw.exception;
 
-import org.springframework.security.core.AuthenticationException;
-
-public class PermissionException extends AuthenticationException {
+public class PermissionException extends RuntimeException {
 
     public static final String DEFAULT_MESSAGE = "요청에 대한 권한이 없습니다.";
 
