ada65ffd4e6188639697074fe8874538e782a375
shoeone96 <dlwnddnjs96@gmail.com>
2024-01-12 12:11:19 +0900

    test: 이미지 성공 케이스 추가 및 예외 케이스 추가

--- src/test/java/com/backendoori/ootw/avatar/service/AvatarItemServiceTest.java
+++ src/test/java/com/backendoori/ootw/avatar/service/AvatarItemServiceTest.java
@@ -5,16 +5,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.stream.Stream;
+import com.backendoori.ootw.avatar.domain.ItemType;
+import com.backendoori.ootw.avatar.domain.Sex;
 import com.backendoori.ootw.avatar.dto.AvatarItemRequest;
 import com.backendoori.ootw.avatar.dto.AvatarItemResponse;
 import com.backendoori.ootw.avatar.repository.AvatarItemRepository;
 import com.backendoori.ootw.common.image.exception.SaveException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -28,7 +32,6 @@ class AvatarItemServiceTest {
     private static final String VALID_TYPE = "HAIR";
     private static final String VALID_SEX = "MALE";
 
-
     @Autowired
     AvatarItemRepository avatarItemRepository;
 
@@ -38,54 +41,75 @@ class AvatarItemServiceTest {
     @Autowired
     MockMvc mockMvc;
 
-    static Stream<Arguments> provideInvalidAvatarImageInfo() {
-        String validType = "HAIR";
-        String validSex = "MALE";
-        return java.util.stream.Stream.of(
-            org.junit.jupiter.params.provider.Arguments.of(null, validSex),
-            org.junit.jupiter.params.provider.Arguments.of(validType, null),
-            org.junit.jupiter.params.provider.Arguments.of("", validSex),
-            org.junit.jupiter.params.provider.Arguments.of(validType, ""),
-            org.junit.jupiter.params.provider.Arguments.of(" ", validSex),
-            org.junit.jupiter.params.provider.Arguments.of(validType, " "),
-            org.junit.jupiter.params.provider.Arguments.of("hair", validSex),
-            org.junit.jupiter.params.provider.Arguments.of(validType, "female")
-        );
-    }
-
     @BeforeEach
     void setup() {
         avatarItemRepository.deleteAll();
     }
 
-    @Test
-    @DisplayName("아바타 이미지 업로드에 성공한다")
-    public void avatarImageTest() {
-        //given
-        MockMultipartFile file = new MockMultipartFile("file", "filename.jpeg",
-            "image/jpeg", "some xml".getBytes());
-        AvatarItemRequest request = new AvatarItemRequest(VALID_TYPE, VALID_SEX);
+    @Nested
+    @DisplayName("아바타 아이템 업로드")
+    class AvatarUpload {
 
-        //when
-        AvatarItemResponse avatarItemResponse = avatarItemService.uploadItem(file, request);
+        @ParameterizedTest(name = "[{index}]: 아이템 타입이 {0}인 경우에 저장에 성공한다.")
+        @ValueSource(strings = {"image/jpeg", "image/gif", "image/png"})
+        @DisplayName("여러 이미지 타입에 대해 업로드에 성공한다")
+        public void avatarImageTest(String contentType) {
+            //given
+            MockMultipartFile file = new MockMultipartFile("file", "filename.jpeg",
+                contentType, "some xml".getBytes());
+            AvatarItemRequest request = new AvatarItemRequest(VALID_TYPE, VALID_SEX);
 
-        //then
-        assertThat(avatarItemResponse.type()).isEqualTo(request.type());
-        assertThat(avatarItemResponse.sex()).isEqualTo(request.sex());
-    }
+            //when
+            AvatarItemResponse avatarItemResponse = avatarItemService.upload(file, request);
 
-    @ParameterizedTest
-    @MethodSource("provideInvalidAvatarImageInfo")
-    @DisplayName("모든 아바타 이미지 조회에 성공한다.")
-    public void getListFailWithWrongRequest(String type, String sex) {
-        //given , when
-        MockMultipartFile file = new MockMultipartFile("file", "filename.jpeg",
-            "image/jpeg", "some xml".getBytes());
-        AvatarItemRequest request = new AvatarItemRequest(type, sex);
+            //then
+            assertThat(avatarItemResponse.type()).isEqualTo(request.type());
+            assertThat(avatarItemResponse.sex()).isEqualTo(request.sex());
+        }
 
-        //then
-        assertThatThrownBy(() -> avatarItemService.uploadItem(file, request))
-            .isInstanceOf(SaveException.class);
+        @ParameterizedTest
+        @MethodSource("provideInvalidAvatarImageInfo")
+        @DisplayName("잘못된 이이템 타입이나 성별을 기입할 시 예외가 발생한다.")
+        public void uploadFailWithWrongRequest(String type, String sex) {
+            //given , when
+            MockMultipartFile file = new MockMultipartFile("file", "filename.jpeg",
+                "image/jpeg", "some xml".getBytes());
+            AvatarItemRequest request = new AvatarItemRequest(type, sex);
+
+            //then
+            assertThatThrownBy(() -> avatarItemService.upload(file, request))
+                .isInstanceOf(SaveException.class);
+
+        }
+
+        @ParameterizedTest(name = "[{index}] content-type 이 {0}인 경우")
+        @ValueSource(strings = {"text/plain", "application/json", "xml"})
+        @DisplayName("파일의 유형이 이미지가 아닌 경우 예외가 발생한다.")
+        public void imageUpLoadWithInvalidContentType(String contentType) throws Exception {
+            //given
+            MockMultipartFile file = new MockMultipartFile("file", "filename.txt",
+                contentType, "some xml".getBytes());
+            AvatarItemRequest request = new AvatarItemRequest(ItemType.TOP.name(), Sex.MALE.name());
+
+            //when, then
+            assertThatThrownBy(() -> avatarItemService.upload(file, request))
+                .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        static Stream<Arguments> provideInvalidAvatarImageInfo() {
+            String validType = "HAIR";
+            String validSex = "MALE";
+            return java.util.stream.Stream.of(
+                org.junit.jupiter.params.provider.Arguments.of(null, validSex),
+                org.junit.jupiter.params.provider.Arguments.of(validType, null),
+                org.junit.jupiter.params.provider.Arguments.of("", validSex),
+                org.junit.jupiter.params.provider.Arguments.of(validType, ""),
+                org.junit.jupiter.params.provider.Arguments.of(" ", validSex),
+                org.junit.jupiter.params.provider.Arguments.of(validType, " "),
+                org.junit.jupiter.params.provider.Arguments.of("hair", validSex),
+                org.junit.jupiter.params.provider.Arguments.of(validType, "female")
+            );
+        }
 
     }
 
@@ -97,7 +121,7 @@ class AvatarItemServiceTest {
             MockMultipartFile file = new MockMultipartFile("file", "filename.jpeg",
                 "image/jpeg", "some xml".getBytes());
             AvatarItemRequest request = new AvatarItemRequest(VALID_TYPE, VALID_SEX);
-            avatarItemService.uploadItem(file, request);
+            avatarItemService.upload(file, request);
         }
 
         List<AvatarItemResponse> list = avatarItemService.getList();
