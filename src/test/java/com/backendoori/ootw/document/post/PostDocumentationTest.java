90bf6e8e6ff5e4faafd8dbfa4714c276954d769f
ASak1104 <khw56184@gmail.com>
2024-01-18 05:07:26 +0900

    test: post create documentation test 추가

---
+++ src/test/java/com/backendoori/ootw/document/post/PostDocumentationTest.java
@@ -0,0 +1,139 @@
+package com.backendoori.ootw.document.post;
+
+import static com.backendoori.ootw.document.common.ApiDocumentUtil.field;
+import static com.backendoori.ootw.document.common.ApiDocumentUtil.getDocumentRequest;
+import static com.backendoori.ootw.document.common.ApiDocumentUtil.getDocumentResponse;
+import static com.backendoori.ootw.security.jwt.JwtAuthenticationFilter.TOKEN_HEADER;
+import static com.backendoori.ootw.security.jwt.JwtAuthenticationFilter.TOKEN_PREFIX;
+import static com.backendoori.ootw.util.provider.ForecastApiCommonRequestSourceProvider.VALID_COORDINATE;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestPartFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+import com.backendoori.ootw.post.dto.request.PostSaveRequest;
+import com.backendoori.ootw.post.dto.response.PostSaveUpdateResponse;
+import com.backendoori.ootw.post.service.PostService;
+import com.backendoori.ootw.security.TokenMockMvcTest;
+import com.backendoori.ootw.weather.domain.TemperatureArrange;
+import com.backendoori.ootw.weather.domain.forecast.ForecastCategory;
+import com.backendoori.ootw.weather.dto.TemperatureArrangeDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import net.datafaker.Faker;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+
+@SpringBootTest
+@AutoConfigureRestDocs
+class PostDocumentationTest extends TokenMockMvcTest {
+
+    static final String API_PREFIX = "/api/v1/posts";
+    static final Faker FAKER = new Faker();
+
+    @MockBean
+    PostService postService;
+
+    @DisplayName("[POST] save 201 Created")
+    @Test
+    void testSaveCreated() throws Exception {
+        // given
+        PostSaveRequest postSaveRequest =
+            new PostSaveRequest(FAKER.book().title(), FAKER.science().element(), VALID_COORDINATE);
+        MockMultipartFile request = getRequestJson(postSaveRequest);
+        MockMultipartFile postImg = getPostImg();
+
+        setToken(1);
+        given(postService.save(postSaveRequest, postImg))
+            .willReturn(new PostSaveUpdateResponse(1L, postSaveRequest.title(), postSaveRequest.content(),
+                FAKER.internet().url(), LocalDateTime.now(), LocalDateTime.now(),
+                TemperatureArrangeDto.from(generateTemperatureArrange())));
+
+        // when
+        ResultActions actions = mockMvc.perform(multipart(API_PREFIX)
+            .file(request)
+            .file(postImg)
+            .header(TOKEN_HEADER, TOKEN_PREFIX + token)
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .accept(MediaType.APPLICATION_JSON)
+            .characterEncoding(StandardCharsets.UTF_8)
+        );
+
+        // then
+        actions.andExpect(status().isCreated())
+            .andDo(
+                document("post-create",
+                    getDocumentRequest(),
+                    getDocumentResponse(),
+                    requestHeaders(
+                        headerWithName("Authorization").description("JWT 토큰")
+                    ),
+                    requestParts(
+                        partWithName("request").description("게시글 생성 요청 정보"),
+                        partWithName("postImg").description("게시글 이미지 파일")
+                    ),
+                    requestPartFields(
+                        "request",
+                        fieldWithPath("title").description("게시글 제목"),
+                        fieldWithPath("content").description("게시글 내용"),
+                        fieldWithPath("coordinate.nx").description("사용자 X 좌표"),
+                        fieldWithPath("coordinate.ny").description("사용자 Y 좌표")
+                    ),
+                    responseFields(
+                        field("postId", JsonFieldType.NUMBER, "게시글 ID"),
+                        field("title", JsonFieldType.STRING, "게시글 제목"),
+                        field("content", JsonFieldType.STRING, "게시글 내용"),
+                        field("image", JsonFieldType.STRING, "게시글 이미지 URL"),
+                        field("createdAt", JsonFieldType.STRING, "게시글 생성 일자"),
+                        field("updatedAt", JsonFieldType.STRING, "게시글 수정 일자"),
+                        field("temperatureArrange.min", JsonFieldType.NUMBER, "최저 기온"),
+                        field("temperatureArrange.max", JsonFieldType.NUMBER, "최고 기온")
+                    )
+                )
+            );
+    }
+
+    private static TemperatureArrange generateTemperatureArrange() {
+        Map<ForecastCategory, String> weatherInfoMap = new HashMap<>();
+        weatherInfoMap.put(ForecastCategory.TMN, String.valueOf(0.0));
+        weatherInfoMap.put(ForecastCategory.TMX, String.valueOf(15.0));
+
+        return TemperatureArrange.from(weatherInfoMap);
+    }
+
+    private MockMultipartFile getRequestJson(PostSaveRequest postSaveRequest) throws JsonProcessingException {
+        return new MockMultipartFile("request", "request.json", MediaType.APPLICATION_JSON_VALUE,
+            objectMapper.writeValueAsBytes(postSaveRequest));
+    }
+
+    private MockMultipartFile getPostImg() {
+        return new MockMultipartFile("postImg", "image.jpeg", MediaType.IMAGE_JPEG_VALUE, "content".getBytes());
+    }
+
+    private RequestPostProcessor setMethod(String method) {
+        return req -> {
+            req.setMethod(method);
+
+            return req;
+        };
+    }
+
+}
